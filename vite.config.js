43ade04f9408ae5dfb4ecaec3115dffe367dfef4
dpipke-setandforget <daniela@setandforget.io>
2023-11-15 06:57:18 -0800

    modify vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   // base: "/gsweet-landing/",
-  server: {
-    headers: {
-      'Content-Security-Policy': "default-src 'none'; worker-src 'self' blob:",
-    },
-  },
 })
